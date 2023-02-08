e74743732f78416ff5a5dfb6f98d5a1469a009e1
Petro Mozil <mozil.petryk@gmail.com>
2023-02-08 13:07:33 +0200

    Add random graph generation

---
+++ graphs/graph_generation.py
@@ -0,0 +1,72 @@
+"""
+The graph generation module
+"""
+
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+from itertools import combinations, groupby
+
+
+def gnp_random_connected_graph(
+    num_of_nodes: int,
+    completeness: int,
+    directed: bool = False,
+    draw: bool = False,
+    filename: str = "graph.png"
+) -> nx.Graph | nx.DiGraph:
+    """
+    Generate a random graph
+
+    Args:
+        num_of_nodes: int - the number of nodes
+        completeness: int - how complete the graph is
+        directed: bool - whether the graph is directed
+        draw: bool - whether to draw the graph
+        filename: str - the filename
+
+    Returns:
+        nx.Graph | nx.DiGraph - the graph
+    """
+    if directed:
+        graph = nx.DiGraph()
+    else:
+        graph = nx.Graph()
+    edges = combinations(range(num_of_nodes), 2)
+    graph.add_nodes_from(range(num_of_nodes))
+
+    for _, node_edges in groupby(edges, key=lambda x: x[0]):
+        node_edges = list(node_edges)
+        random_edge = random.choice(node_edges)
+        if random.random() < 0.5:
+            random_edge = random_edge[::-1]
+        graph.add_edge(*random_edge)
+        for e in node_edges:
+            if random.random() < completeness:
+                graph.add_edge(*e)
+
+    for (source, dest, weight) in graph.edges(data=True):
+        weight['weight'] = random.randint(-5, 20)
+
+    if draw:
+        plt.figure(figsize=(10, 6))
+        if directed:
+            pos = nx.arf_layout(graph)
+            nx.draw(graph, pos, node_color='lightblue',
+                    with_labels=True,
+                    node_size=500,
+                    arrowsize=20,
+                    arrows=True)
+            labels = nx.get_edge_attributes(graph, 'weight')
+            nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
+
+        else:
+            nx.draw(
+                graph,
+                node_color='lightblue',
+                with_labels=True,
+                node_size=500
+            )
+        plt.savefig(filename)
+        
+    return graph
