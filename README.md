52accc83849dfb52ebb1fe47ad415c380550edc5
Petro Mozil <mozil.petryk@gmail.com>
2023-02-19 12:18:42 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Graph algorithms and decision trees
 [![Open in Colaboratory](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/pmozil/discrete_lab_3/blob/main/report.ipynb)
+
+The code for the discrete mathematics lab.
+
+Authored by pmozil <mozil.petryk@gmail.com> and 
+
+The lab consists of five algorithms, four of them related to graphs, while the last one is a decision tree fitter.
+
+These are:
+- Prim's algorithm
+- Kruskal's algorithm
+- Floyd-Warshall algorithm
+- Bellman-Ford's algorithm
+- The decision tree
+
+To view the full report, go to colaboratory, link for which is in the badge above.
--- graphs/prim.py
+++ graphs/prim.py
@@ -78,41 +78,41 @@ if __name__ == "__main__":
     all_edges: set[tuple[int, int]] = set(
         (edge[0], edge[1]) for edge in random_graph.edges()
     )
-    # fig, ax = plt.subplots()
+    fig, ax = plt.subplots()
 
-#     def animate(edges: set[tuple[int, int]]) -> None:
-#         """
-#         Make a step in an animation.
+    def animate(edges: set[tuple[int, int]]) -> None:
+        """
+        Make a step in an animation.
 
-#         Args:
-#             edges: list[tuple[int, int]] - the list of edges
-#         """
-#         ax.clear()
-#         nx.draw_networkx_nodes(random_graph, pos, node_size=25, ax=ax)
-#         nx.draw_networkx_edges(
-#             random_graph,
-#             pos,
-#             edgelist=list(all_edges.difference(edges)),
-#             alpha=0.1,
-#             edge_color="g",
-#             width=1,
-#             ax=ax,
-#         )
-#         nx.draw_networkx_edges(
-#             random_graph,
-#             pos,
-#             edgelist=list(edges),
-#             alpha=1.0,
-#             edge_color="b",
-#             width=1,
-#             ax=ax,
-#         )
+        Args:
+            edges: list[tuple[int, int]] - the list of edges
+        """
+        ax.clear()
+        nx.draw_networkx_nodes(random_graph, pos, node_size=25, ax=ax)
+        nx.draw_networkx_edges(
+            random_graph,
+            pos,
+            edgelist=list(all_edges.difference(edges)),
+            alpha=0.1,
+            edge_color="g",
+            width=1,
+            ax=ax,
+        )
+        nx.draw_networkx_edges(
+            random_graph,
+            pos,
+            edgelist=list(edges),
+            alpha=1.0,
+            edge_color="b",
+            width=1,
+            ax=ax,
+        )
 
-#     anim = animation.FuncAnimation(
-#         fig,
-#         animate,
-#         init_func=lambda: None,
-#         frames=lambda: prim_with_yielding(random_graph),
-#         interval=500,
-#     )
-#     plt.show()
+    anim = animation.FuncAnimation(
+        fig,
+        animate,
+        init_func=lambda: None,
+        frames=lambda: prim_with_yielding(random_graph),
+        interval=500,
+    )
+    plt.show()
